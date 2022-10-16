cea71498d888b19aa3f1097a2a4b4b388b02e48c
Diego Lopez <lopezramos@chapman.edu>
2022-10-15 23:24:45 -0700

    Fixed parser

--- parser.py
+++ parser.py
@@ -1,31 +1,28 @@
-import sys
-
 def parseOutput():
     output = {
-        "Drive Name" : "",
-        "Engine" : "",
-        "Scanned Directories" : "",
-        "Scanned Files" : "",
-        "Infected Files" : "",
-        "Scan duration" : ""
+        "Drive name" : "",
+        "Engine version" : "",
+        "Scanned directories" : "",
+        "Infected files" : "",
+        "Data scanned" : "",
+        "Time" : ""
     }
 
-    # data = sys.stdin.readline()
     with open('ex.txt') as f:
     # read every line in data
-        for key, line in zip(output, f):
-            # save lines that contain a key in the dictionary
-            if key in line:
-                output[key] = line[len(key):]
-            # print("Key: ", key, ", Line: ", line)
-
+        for line in f:
+            for key in output:
+                # save lines that contain a key in the dictionary
+                if key in line:
+                    output[key] = line[len(key):]
+                    break
 
     return output
 
 def main():
     scandict = parseOutput()
     for key in scandict:
-        print(key, ": ", scandict[key])
+        print(key, scandict[key])
 
 
 if __name__ == "__main__":
