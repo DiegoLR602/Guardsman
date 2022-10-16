0003cf18d9e3d0233e4b99796e3880f52e4bee39
natanielf <natanielfarzan@gmail.com>
2022-10-15 23:40:09 -0700

    Script updates

--- src/createReport.sh
+++ src/createReport.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-FILE=./reportfile.txt
+# It is expected that this file is located in /usr/local/bin/
+
+DIR=$1
+FILE=/scans/reportfile.txt
 
 if test -f "$FILE"; then
-	echo "Report file exists, cp,learing content."
+	echo "Report file exists, clearing content."
 	> $FILE
 else
+	mkdir -p /scans
 	install -m 755 ./reportfile.txt
-	echo "$FILE was cerated."
+	echo "$FILE was created."
 fi
 
 echo -e "\nScanning drive...\n"
 
 echo -e "**************USB GUARDSMAN REPORT***************\n" >> $FILE
 
-clamscan -r / /Users/diegolopezramos/Documents/CPSC_Courses/CS408 | tee -a ./reportfile.txt
+clamscan -r $DIR | tee -a /scans/reportfile.txt
